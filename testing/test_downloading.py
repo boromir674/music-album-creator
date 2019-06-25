b84ea71dcadfc2c7a8ea4785dff785e21e26e2ff
Konstantinos <>
2019-06-26 01:40:26 +0200

    Fix 'test_downloading' unittest

--- testing/test_downloading.py
+++ testing/test_downloading.py
@@ -1,7 +1,7 @@
 import os
 import pytest
 
-from music_album_creation.downloading import YoutubeDownloader, WrongYoutubeUrl
+from music_album_creation.downloading import YoutubeDownloader, IUR, WUE
 
 
 @pytest.fixture(scope='module')
@@ -11,12 +11,17 @@ def youtube():
 
 class TestYoutubeDownloader:
     NON_EXISTANT_YOUTUBE_URL = 'https://www.youtube.com/watch?v=alpharegavgav'
+    INVALID_URL = 'gav'
     duration = '3:43'
     duration_in_seconds = 223
 
-    def test_downloading_false_url(self, youtube):
-        with pytest.raises(WrongYoutubeUrl):
+    def test_downloading_false_youtube_url(self, youtube):
+        with pytest.raises(WUE):
             youtube.download(self.NON_EXISTANT_YOUTUBE_URL, '/tmp/', spawn=False, verbose=False, supress_stdout=True)
+
+    def test_downloading_invalid_url(self, youtube):
+        with pytest.raises(IUR):
+            youtube.download(self.INVALID_URL, '/tmp/', spawn=False, verbose=False, supress_stdout=True)
 
     @pytest.mark.parametrize("url, target_file", [('https://www.youtube.com/watch?v=Q3dvbM6Pias', 'Rage Against The Machine - Testify')])
     def test_downloading_valid_url(self, url, target_file, youtube):
